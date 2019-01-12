aa161cc4a15f76a7f5aba3e8b133af573d65a75e
Kevin O'Brien <kob123@hotmail.co.uk>
2019-01-12 11:09:32 +0000

    Refactoring data loading to lifecycle methods

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,29 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	// The constructor method is called immediately when an instance of
-	// the class is created. Therefore its a good place to initialize state.
-	constructor(props) {
-		super(props);
+// 	constructor(props) {
+// 		super(props);
 
-		this.state = { 
-			lat: null,
-			errorMessage: ''
-		};
+// 		this.state = { 
+// 			lat: null,
+// 			errorMessage: ''
+// 		};
+// }
 
-		// getCurrentPosition() takes two callback functions: first is called if 
-		// it gets a poition, second is called if not.
-		// The callback functions run asynchronously, i.e. they run when getCurrentPosition() 
-		// gets or doesn't get a position, not when the constructor method calls getCurrentPosition().
-		// You can see the delay: 'Latitude:' is rendered immediately, but the number only after a 
-		// couple of seconds.
+state = { lat: null, errorMessage: ''};
+
+componentDidMount() {
 		window.navigator.geolocation.getCurrentPosition(
-			position => {
-				this.setState({ lat: position.coords.latitude });
-			},
-			err => {
-				this.setState({ errorMessage: err.message} );
-			}
+			position => this.setState({ lat: position.coords.latitude }),
+			err => this.setState({ errorMessage: err.message})
 	);
 }
 	
