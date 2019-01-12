835885898095eb63a05e8bd11a7c811d2a6eb9f9
Kevin O'Brien <kob123@hotmail.co.uk>
2019-01-12 13:47:01 +0000

    Extracting options to config objects

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+	winter: {
+		text: 'Burr, it is chilly',
+		iconName: 'snowflake'
+	},
+	summer: {
+		text: 'Lets hit the beach',
+		iconName: 'sun'
+	}
+}
+
 const getSeason = (lat, month)=> {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
@@ -10,11 +21,19 @@ const getSeason = (lat, month)=> {
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+		// Destructure the text and iconName property values into same name variables.
+		// Variable names have to be same as object property names.
+		const {text, iconName} = seasonConfig[season];
+
+		// Don't need same names if not destructuring. Can combine bracket and dot notation.
+		// const texty = seasonConfig[season].text;
+		// const iconNamey = seasonConfig[season].iconName;
 
     return (
 			<div>
+				<i className={`${iconName} icon`} />
 				<h1>{text}</h1>
+				<i className={`${iconName} icon`} />
 			</div>
 		);
 };
