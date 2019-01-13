e21e926225ac312eef9df1b1c05dc5623289cc21
Kevin O'Brien <kob123@hotmail.co.uk>
2019-01-13 10:01:53 +0000

    Using helper function to avoid conditionals in Render

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -13,15 +13,28 @@ componentDidMount() {
 			err => this.setState({ errorMessage: err.message})
 	);
 }
-	
+
+// A 'helper function'. Use them to avoid having conditional logic in 
+// the render method.
+renderContent() {
+  if (this.state.errorMessage && !this.state.lat) {
+    return <div>Error: {this.state.errorMessage}</div>
+  }
+  if (!this.state.errorMessage && this.state.lat) {
+    return <SeasonDisplay lat={this.state.lat} />;
+  }
+  return <Spinner message="Please accept location request"/>
+}
+
+// There is no 'border red' class, it's just an example of a change you might 
+// need to make to what you render. If you didn't put the conditional logic in 
+// a helper function you'd have to apply the border class to each condition.
 	render() {
-			if (this.state.errorMessage && !this.state.lat) {
-				return <div>Error: {this.state.errorMessage}</div>
-			}
-			if (!this.state.errorMessage && this.state.lat) {
-				return <SeasonDisplay lat={this.state.lat} />;
-			}
-			return <Spinner message="Please accept location request"/>
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
   };
 }
 
